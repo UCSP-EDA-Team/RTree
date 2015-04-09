33d12ea0958add009ad5b0bedd9daa5b24a87209
Juan Alfredo Salas Santillana <juan.salas.santillana@ucsp.edu.pe>
2015-04-08 19:47:43 -0500

    Change function name find to chooseLeaf

--- RTreeA.pro
+++ RTreeA.pro
@@ -10,6 +10,7 @@ QT       -= gui
 
 TARGET = RTreeA
 CONFIG   += console
+CONFIG += c++11
 CONFIG   -= app_bundle
 
 TEMPLATE = app
--- Tree.h
+++ Tree.h
@@ -11,41 +11,59 @@ private:
     {
         friend class Tree;
     private:
-        Object * objects[CHILDS];
-        Node * childs[CHILDS];
-
+        Node * parent;
         size_t level;
+
+        vector<Object> objects;
+        vector<Node *> childs;
     public:
+        Node(Node *parent, size_t level)
+        {
+            this->parent = parent;
+            this->level = level;
+        }
     };
     
     Node * head;
 
 public:
+    Tree()
+    {
+        this->head = new Node(nullptr,0);
+    }
+
     bool insert(Object data)
     {
       Node ** s;
-      if(this->find(data, s))return false;
+      this->chooseLeaf(data, s);
+
+      if((*s)->objects.size() < CHILDS)add(data, s);
 
       //*s = new Node(data);
 
       return true;
     }
 
-    bool find(Object data, Node** &s)
+    bool add(Object data, Node** &s)
+    {
+        (*s)->objects.push_back(data);
+    }
+
+    bool chooseLeaf(Object data, Node** &s)
     {
         s = &this->head;
         double min;
         size_t use;
-        while(*s){
+        while((*s)->level!=0){
             min = INF;
             use = 0;
-            for(int i=0;i<CHILDS;i++)
-                if((*s)->objects[i]->contain(data) < min)
+            for(int i=0;i<(*s)->objects.size();i++)
+                if((*s)->objects[i].contain(data) < min)
                     use = i;
 
             s = &(*s)->childs[use];
         }
-        return *s;
+        return true;
     }
     
 };
