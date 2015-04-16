1e16c35c05d096a24e1ea711917611fe089d06a7
Jose Manuel <jose.valdivia.romero@ucsp.edu.pe>
2015-04-16 02:45:09 -0500

    Fixing

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <QCoreApplication>
 #include <Tree.h>
-#include <iostream>
-using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
 
     return a.exec();
 }
