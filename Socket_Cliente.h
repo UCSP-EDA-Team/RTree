79dd5e7a73dddc459b2a0844ac8a10c48701c096
jmme21 <jmme21@gmail.com>
2015-04-25 08:20:56 -0500

    creando socket cliente
    abre conexion

---
+++ Socket_Cliente.h
@@ -0,0 +1,67 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+int Abre_conexion_Inet_remote(char *Host_Servidor, char *Servicio){
+	struct addrinfo hints, *res;
+	int sockfd;
+
+	memset(&hints, 0, sizeof hints);
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+
+	getaddrinfo(Host_Servidor, Servicio, &hints, &res);
+
+
+	sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+
+
+	if(connect(sockfd, res->ai_addr, res->ai_addrlen)==-1)
+		return -1;
+	return sockfd;
+}
+
+int Abre_Conexion_Inet (
+	char *Host_Servidor, 
+	char *Servicio)
+{
+	struct sockaddr_in Direccion;
+	struct servent *Puerto;
+	struct hostent *Host;
+	int Descriptor;
+
+	Puerto = getservbyname (Servicio, "tcp");
+	if (Puerto == NULL)
+		return -1;
+
+	Host = gethostbyname (Host_Servidor);
+	if (Host == NULL)
+		return -1;
+
+	Direccion.sin_family = AF_INET;
+	Direccion.sin_addr.s_addr = ((struct in_addr *)(Host->h_addr))->s_addr;
+	Direccion.sin_port = Puerto->s_port;
+	
+	Descriptor = socket (AF_INET, SOCK_STREAM, 0);
+	if (Descriptor == -1)
+		return -1;
+
+	if (connect (
+			Descriptor, 
+			(struct sockaddr *)&Direccion, 
+			sizeof (Direccion)) == -1)
+	{
+		return -1;
+	}
+
+	return Descriptor;
+}
+
+
